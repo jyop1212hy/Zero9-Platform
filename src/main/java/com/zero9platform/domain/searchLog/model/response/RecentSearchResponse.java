5cb838a829cae165167a6f53675ccafe36191bc9
jyop1212 <jyop1114@gmail.com>
2026-02-14 10:45:51 +0900

    fix: RecentSearchResponse.java 역직렬화 문제 수정

--- src/main/java/com/zero9platform/domain/searchLog/model/response/RecentSearchResponse.java
+++ src/main/java/com/zero9platform/domain/searchLog/model/response/RecentSearchResponse.java
@@ -2,13 +2,12 @@ package com.zero9platform.domain.searchLog.model.response;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
-
 import java.time.LocalDateTime;
 
 @Getter
-@RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
-@JsonPropertyOrder({ "keyword", "createdAt" })
+@RequiredArgsConstructor
+@JsonPropertyOrder({ "keyword", "searchedAt" })
 public class RecentSearchResponse {
 
     private final String keyword;
